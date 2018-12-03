bbc7b1ebb8696b8547d25f3200c1fa4328fa3ed6
zmontgo <31627113+zmontgo@users.noreply.github.com>
2018-12-02 21:20:56 -0600

    Updated first files.

---
+++ day_1/puzzle0.php
@@ -0,0 +1,318 @@
+<?php
+  // The ids
+  $ids = "dghfbsyiznoumojleevappwqtr
+dghfbsyiznoumkjljevacpmqer
+dghfbbyizioumkjlxevacpwdtr
+dghfbsesznoumkjlxevacpwqkr
+dghfbsynznoumkjlxziacpwqtr
+cghfbsyiznjumkjlxevacprqtr
+dghfjsyizwoumkjlxevactwqtr
+dghfdsyfinoumkjlxevacpwqtr
+hghfbsyiznoumkjlxivacpwqtj
+dgcfbsyiznoumkjlxevacbuqtr
+dghfbsyiznoymnjlxevacpwvtr
+dfhfbsyiznoumkulxevacptqtr
+dghfasyiznovmkjlxevacpwqnr
+dghfbsyihnouikjlxevackwqtr
+dghfbayiznolmkjlyevacpwqtr
+jghfbsyiznoumnjldevacpwqtr
+dhhfbsyuznoumkjlxevakpwqtr
+nehfrsyiznoumkjlxevacpwqtr
+dghfbsyiznxdmkolxevacpwqtr
+dgpfbsyizwlumkjlxevacpwqtr
+yghfbsyiznoumkjlsevacpwqtm
+dghfssyiznoumkjlxevvcpwqjr
+dahfbsyiznoumkjlfevacpwqto
+duhfcsyiznouvkjlxevacpwqtr
+dghfbvyiznoumkjlrevacpwvtr
+dghfgsyiznoumknlxgvacpwqtr
+jghfbeyiznkumkjlxevacpwqtr
+daofbsyiznoumkjlxevampwqtr
+dghfbsyiznojmkjlxeracpcqtr
+dghnbsyiznouokjlxevaclwqtr
+dgifbsyiznoumkjlxevnspwqtr
+dgkfpsziznoumkjlxevacpwqtr
+dghfxsyijnoumkjlxevaccwqtr
+dghfbsyiznolmkjlwevzcpwqtr
+dkhfbsaiznoumkjlxevacpwqtg
+dghfbsygknoumkjlaevacpwqtr
+dghfbsyizizumkjlxevacpxqtr
+ighfbbyijnoumxjlxevacpwqtr
+dghfbsyizrouekjlxevacpwktr
+dghobsyiznoujkjlxevacnwqtr
+dghpbsyizyoumkjlxeaacpwqtr
+dghffsyiznoymkjlxevacewqtr
+dghkbssiznoumzjlxevacpwqtr
+dghfbsyawnoumkjlxevacpwjtr
+drhfbsyiznormkjlfevacpwqtr
+dghfbsoiznouwkjlxevacpwqtn
+dghfmsyiznoumkjlxlvecpwqtr
+dxhfbsyiznoumkjlxeeacvwqtr
+dghnbsyiznoumkjsxevacpwqur
+dghfbsyiznrujkjlxevacpwqtc
+dghfbstoznoumhjlxevacpwqtr
+dghfboyiznzumkjlvevacpwqtr
+dghfbsyiznjumkjlxevgcpwmtr
+dghfbsnizaoumkjlxevacpwetr
+dghfbsyirnoumkjoxevacplqtr
+dghfbsyiznoumkjlxavvckwqtr
+dghfbjdiznoumkjlxevacpwptr
+dghfbsywznoumkjlxeiacpwqcr
+djhfbsyizuoumkjlxelacpwqtr
+dghffsniznoumkjlxpvacpwqtr
+dghebsyizuoumkjlxevecpwqtr
+rghfbsyiznourkjcxevacpwqtr
+dghfbsyignoumkwlxenacpwqtr
+dghfbsyiznrufkjlxevacpwqth
+dgifbsyiznoumkjlxevacpjqnr
+dghfbsyiznoumkjbxevaxpwqtw
+dsufbsyizncumkjlxevacpwqtr
+dihfbsyiznoumujlxecacpwqtr
+dghfbiyiznoumkjlxevajpwqtn
+dghqbsyixnoumkjlrevacpwqtr
+dghfbsyiznouukjlxeuacpwqtx
+dghfbsyizyoumksfxevacpwqtr
+dghfbsiiznopfkjlxevacpwqtr
+eghfbsyidnoumkjlxexacpwqtr
+dghfbgyiznouwkjlwevacpwqtr
+dghfbsyyznoumkjlxevacwwqtf
+bghfbtypznoumkjlxevacpwqtr
+dghfbsyiznoumtjlxebacpwetr
+dghfbsgiznonmkplxevacpwqtr
+dghfbsyiznoumxjlxevanpwqpr
+dghfbsyiznwumujuxevacpwqtr
+dghxbsyiznoumkjzxevaypwqtr
+dghfbsyhznoumkjlxlvacpiqtr
+dghfbsyiznoumkjlxevzcnwqrr
+dvhfbsyiznoumkjluevacpzqtr
+dghcbsyiznoumkjlxmvacpwetr
+dghfbsyiznohmkjvxbvacpwqtr
+dghfzsyiznouokjlxevacpwqpr
+dghfbsyiznoumkjlxevachtqth
+dghfbsyiznoumkjlxjvacpfutr
+dghfbsyiznoumkjlxevsppwqtt
+dghfusyiznouakhlxevacpwqtr
+dghfbsyizcoumkjlxrvaipwqtr
+dghebsyipnoumfjlxevacpwqtr
+dgdfbsyiznoumkjlwevacpkqtr
+dghfbsyiznoumkjlcffacpwqtr
+dghfbsypznfumkjlxevacpwqar
+dghfbsyiznojmkjlxevgcpkqtr
+dghfbsyiznoumkjlaevlcpwstr
+dgafrsyiunoumkjlxevacpwqtr
+dghfbsyiznouqljlxevacrwqtr
+dyhkbsyiznokmkjlxevacpwqtr
+pghfbsciznoumkjlxevacpwvtr
+dghfbxyiznonmkjllevacpwqtr
+ighfbsyizxoumkjlxevacpzqtr
+dgffbsyoznoumkjlxevacpwqto
+hghfbsyiznoumkjlpevachwqtr
+dlhfosyiznoumkjldevacpwqtr
+dghfbsvizkoumkjlxvvacpwqtr
+dbafbsyiznozmkjlxevacpwqtr
+dghfbsyidnoumkjlxrvjcpwqtr
+dghfbsyiznfumkjlxeqacpwqta
+dghfbsfiznoumkjvxevacjwqtr
+dghfbsyimnoumrjlhevacpwqtr
+dghflsyiznoumkjlxevacpvqmr
+dghfbmfiznoumkjlxevacpdqtr
+dghfbsyizsouzkjlxevscpwqtr
+dghfksyiznoumimlxevacpwqtr
+dghfbsyiznoumkjlxevbwpwqur
+wghcbsyiznoumkjlkevacpwqtr
+kghfbioiznoumkjlxevacpwqtr
+dghfbsiizeoumkjlxmvacpwqtr
+dglfbsyilnoumkjlxevpcpwqtr
+dgqfbsylznoumkjlxevacpwqcr
+dglfhsyiznoumkjlxevacpwqdr
+dghfbsymznoumkjlxbvacpwqtb
+hghfbsyizhoumkjlxtvacpwqtr
+dghdbsyiznoumkjlxeiacpyqtr
+dohfbsyiznoumkjmxlvacpwqtr
+xhhfbsyiznoumkjlxegacpwqtr
+dlhfbsyiznoumkjlxnvahpwqtr
+dghfbsyiznovdpjlxevacpwqtr
+dgcfbsyiznoumkjlxevactwqdr
+dghfksyiknoumkjlxevacpwqcr
+ughfqsyiznoumkjlxevacpwctr
+dghfbjyiznoumkjlxsvacnwqtr
+dgwfbagiznoumkjlxevacpwqtr
+dghfbsyiznoumknlxevtcpwqdr
+jghfksyiznoumkjlxeoacpwqtr
+dghfbsyiznoimkjlwezacpwqtr
+dghfbsyiunoumkjlxeqacpwstr
+dghfbsyizjoumkwlxevaypwqtr
+dghfysriznoumkjlxevucpwqtr
+dghfbsygzjoumkjfxevacpwqtr
+dghfbhviznoumkjlxevacpwqtq
+dghfbsyiznoumkjvwevacpwqur
+dghfbsyiznoumtjlxevacplqnr
+yghfbsysznouykjlxevacpwqtr
+dgwfbsiiznoumkjlxevacfwqtr
+dghfbsyizooumkjlxevampiqtr
+dshfbsyiznoumkjlxevawpoqtr
+dghtbsyxznuumkjlxevacpwqtr
+dkhfblyiznoumkjlxevacpaqtr
+dgkfbsyiinoumkjlxegacpwqtr
+dghfbtxiznouhkjlxevacpwqtr
+dghfbsyiznoumkjlxkvmcpeqtr
+dghfbsyiznoumkjlghvacpwqmr
+dghfbsbizioumkjlcevacpwqtr
+dphfbsyizhoumkjwxevacpwqtr
+dghfbsyiznqumkjlugvacpwqtr
+dghfbsjinnoumkjlxevacpwetr
+mghfbsyiznoumkjlxfvacpjqtr
+dghfbsxiznoumkjlxetacwwqtr
+dghmbsyiznoumbjlxevacpwqyr
+dghfbsyiznwumkjlwevacmwqtr
+dgkfbsyiznotmkjlxevacpwstr
+dghfbsyiznouykjlxeiacuwqtr
+dghfbsynznbhmkjlxevacpwqtr
+dgyfbsyiznoumtjlbevacpwqtr
+dghfbftiznoumkjlxevacpwatr
+dghfvsyiznouikjlievacpwqtr
+dghfbsyiznodmkjlxevncpwqtz
+yfhfbsyiznoumkjluevacpwqtr
+dghfbzyiznoumhflxevacpwqtr
+dphfbsyizncumkjlxevacpwqtf
+dghfasyiznoumkjlxeaicpwqtr
+dgffbsyiznoumkjlzevacpwqsr
+dghfbsyiznoumkmxxcvacpwqtr
+dghffsyiznoumkjlxevacpwqre
+dghfbsyizndmmkjlxemacpwqtr
+dghfbsviznoamkjlxevappwqtr
+dghfbsyiznouckrlxevacpdqtr
+dgwfbsyiznyumkjlxevacpqqtr
+dujfbsyiznoumgjlxevacpwqtr
+dghobsailnoumkjlxevacpwqtr
+dghfkqyiznoumknlxevacpwqtr
+dghfbyypznoumkjlxevacpwatr
+wqhfbsyiznoumkjlxevzcpwqtr
+dghfbsyiznoumwjlxrvacppqtr
+dghfbsymznoumkflxevacplqtr
+dghfbsyiznounkjpgevacpwqtr
+ighfbsyijnoumxjlxevacpwqtr
+dghfbsyizroumkjllevncpwqtr
+dghfbsliznokmkjlxevacpwqtb
+dgefbsyiznoumkqlxevpcpwqtr
+dghfbtypznouzkjlxevacpwqtr
+dmhfbsyiznoumkjlxeyactwqtr
+vohfbsyiznoumkjlqevacpwqtr
+dgsfpsyiznodmkjlxevacpwqtr
+dghfzsyijnoumkjnxevacpwqtr
+dghfbayijroumkjlxevacpwqtr
+dghfbsyiznodmxjlxgvacpwqtr
+dghfbsyiznocmkjlxhvaipwqtr
+dghmbsyignoumkjlxevacpoqtr
+dghfbsyiznosmkjlncvacpwqtr
+dggfbsyiznuumkjlxevacpwqrr
+dghibsyilnoumkjlxevacowqtr
+dghfbsyiznoumkjluevbcowqtr
+dghfbsaiznyuvkjlxevacpwqtr
+dgnfxsyiznommkjlxevacpwqtr
+dghfbnyiznoumkjlsnvacpwqtr
+dghfssiiznoumkjlxavacpwqtr
+dghfbsyizneumajlxevacfwqtr
+dghfbsyiznoumkjlxevycpvptr
+qghfbsyizgoumkjlxevacpwttr
+vghfbsyiznoumkjlievaepwqtr
+dghfbsyiznoumejlxjvacpwdtr
+dghfbsyispoumkjlxevacpwqtg
+duhfbsyizpoumkjlxenacpwqtr
+dghfbsyifnoumkblxevacpnqtr
+bghfbsyxznoumkjleevacpwqtr
+dgtfbsyzpnoumkjlxevacpwqtr
+dghfbsyiznoumkjlsecacpwqth
+dghfqsyiznjumkjlxevawpwqtr
+dgcfbsyizboumkjlxevacqwqtr
+dghfbqyiznoumkjkxevacpwqtj
+dgyfbsyfznoumkjlievacpwqtr
+dghfdsyiznoumkplxevacpwdtr
+dphfbsyuznkumkjlxevacpwqtr
+dghfbsyiznoupkjitevacpwqtr
+dghfisyiznoamkjlxevacpwqwr
+dgufbsyiznoumkjlxivvcpwqtr
+dghfbvyiznoumkjlxevacvwqtz
+dghfbsyiqnxumkjlxbvacpwqtr
+dghubsyiznqumkflxevacpwqtr
+dghfbsyiznzumkjlxevacpdbtr
+dghfbsyiznoumkjlxehacpwwrr
+mghfbsyiznoumkjlxevacpwqbp
+dvhfbryiznoumkclxevacpwqtr
+dghbbsyiznotmkjlxevacpwqhr
+dghfrsyiznoomkjlxevacpwqto
+dghfbkyiznoumkjlxeracpxqtr
+dghfbfyizfoumkjlxevacpwjtr
+dghfbsyizqoulkjlxevacpwqtt
+dghfbsyiwnoumkjlxevacxwgtr
+dghfbsyiznormkjlgxvacpwqtr
+dghybsyizioumkjoxevacpwqtr
+dchfbsyiznoumkjlxyvacpwqtc
+dgyfbsyiznouckjlxewacpwqtr
+dakfbsyeznoumkjlxevacpwqtr";
+
+$alphabet = array(
+  1 => "a",
+  2 => "b",
+  3 => "c",
+  4 => "d",
+  5 => "e",
+  6 => "f",
+  7 => "g",
+  8 => "h",
+  9 => "i",
+  10 => "j",
+  11 => "k",
+  12 => "l",
+  13 => "m",
+  14 => "n",
+  15 => "o",
+  16 => "p",
+  17 => "q",
+  18 => "r",
+  19 => "s",
+  20 => "t",
+  21 => "u",
+  22 => "v",
+  23 => "w",
+  24 => "x",
+  25 => "y",
+  26 => "z",
+);
+
+  // Make above string into an array
+  $array = explode(PHP_EOL, $ids);
+
+  $dups = 0;
+
+  $trips = 0;
+
+  $duplicate = false;
+
+  $triple = false;
+
+  $num = 0;
+
+  foreach($array as $key => $val) {
+    foreach($alphabet as $letter) {
+      $remove = false;
+      if ((substr_count ($val, $letter) == 2) && ($duplicate == false)) {
+        $dups += 1;
+        $duplicate = true;
+        $remove == true;
+      }
+      if ((substr_count ($val, $letter) == 3) && ($triple == false)) {
+        $trips += 1;
+        $triple = true;
+        $remove == true;
+      }
+    }
+    if($remove == true) {
+      unset($array[$key]);
+    }
+    $triple = false;
+    $duplicate = false;
+    $num += 1;
+  }
+
+  echo $dups * $trips;
